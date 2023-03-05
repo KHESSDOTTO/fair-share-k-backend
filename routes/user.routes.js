7b0a65b3de9fda238640e6b71784205270c997a6
Khess S Dotto <khessdotto@gmail.com>
2023-03-04 21:06:32 -0300

    CRUD Users done (getUser, editUser, deleteUser, createUser, Login).

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,6 @@ import express from "express";
 import { generateToken } from "../config/jwt.config.js";
 import isAuth from "../middlewares/isAuth.js";
 import attachCurrentUser from "../middlewares/attachCurrentUser.js";
-import { isClient } from "../middlewares/isClient.js";
-import { isBusiness } from "../middlewares/isBusiness.js";
 import { UserModel } from "../model/user.model.js";
 import bcrypt from "bcrypt";
 
@@ -30,6 +28,7 @@ userRouter.post("/signup", async (req, res) => {
       passwordHash: hashedPassword,
     });
     delete createdUser._doc.passwordHash;
+    delete createdUser._doc.favorites;
     return res.status(201).json(createdUser);
   } catch (err) {
     console.log(err);
@@ -46,19 +45,52 @@ userRouter.post("/login", async (req, res) => {
     }
     if (await bcrypt.compare(password, user.passwordHash)) {
       const token = generateToken(user);
+      delete user._doc.passwordHash;
+      delete user._doc.favorites;
       return res.status(200).json({
-        // user: {
-        //   name: user.name,
-        //   email: user.email,
-        //   _id: user._id,
-        //   type: user.type,
-        // },
         user: { ...user._doc },
         token: token,
       });
     } else {
       return res.status(401).json({ msg: "Email ou senha invalidos." });
     }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+userRouter.get("/get", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const cUser = await UserModel.findById(req.currentUser._id);
+    delete cUser._doc.passwordHash;
+    return res.status(200).json(cUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+userRouter.put("/edit", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.currentUser._id,
+      { ...req.body },
+      { runValidators: true, new: true }
+    );
+    delete updatedUser._doc.passwordHash;
+    return res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+userRouter.delete("/delete", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(req.currentUser._id);
+    delete deletedUser._doc.passwordHash;
+    return res.status(200).json(deletedUser);
   } catch (err) {
     console.log(err);
     return res.status(500).json(err);
