7d306b8c46c93e2f802e3d0dbaa5e6e78c414abc
Khess S Dotto <khessdotto@gmail.com>
2023-03-05 14:07:20 -0300

    Added description property to productModel. Fixed bugs from product routes. Product routes tested and working.

--- model/product.model.js
+++ model/product.model.js
@@ -4,6 +4,7 @@ const productSchema = new Schema({
   name: { type: String, required: true },
   picture: { type: String, required: true },
   price: { type: Number, required: true },
+  description: { type: String, required: true },
   expirationDate: { type: Date, required: true },
   creator: { type: String, required: true, ref: "User" },
 });
