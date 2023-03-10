ad1f16064a0766a0f9afe3116d0701626421e7fd
Khess S Dotto <khessdotto@gmail.com>
2023-03-10 13:01:21 -0300

    Activated nodemailer again (send confirmation emails).

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -49,7 +49,7 @@ userRouter.post("/signup", async (req, res) => {
       subject: "Activate account",
       html: `<p>Click here to activate your account:<p> <a href=http://localhost:${process.env.PORT}/api/user/activate-account/${createdUser._id}>CLICK HERE</a>`,
     };
-    // await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
     return res.status(201).json(createdUser);
   } catch (err) {
     console.log(err);
